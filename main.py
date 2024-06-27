afa5acd88d560980f21fc49f2126ba6137d8d0a8
KamilloDev <krystiankowalczyk1204@gmail.com>
2024-06-27 17:31:51 +0200

    Made a cookie consent pop-up agremment

--- main.py
+++ main.py
@@ -15,3 +15,15 @@ driver = webdriver.Chrome(options=options)
 # Define the login page URL and the target URL after login
 login_page_url = "https://boulders.goactivebooking.com/login"
 target_page_url = "https://boulders.goactivebooking.com/my-pages"
+
+# Open the login page
+driver.get(login_page_url)
+
+# Wait for the cookie consent pop-up to be present
+try:
+    cookie_accept_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="BRP_MODAL_CONTAINER"]/div[2]/div[2]/div/div/div[4]/div[1]/div[2]'))  # Update the XPath as needed
+    )
+    cookie_accept_button.click()
+except TimeoutException:
+    print("No cookie consent pop-up found or it took too long to load.")
