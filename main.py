d567d3c9644a3cf324c9b6892fa24526fb1db512
KamilloDev <krystiankowalczyk1204@gmail.com>
2024-06-27 17:52:42 +0200

    Added so that the program can counter the remember and pop-up and waits for the login elements to load

--- main.py
+++ main.py
@@ -27,3 +27,22 @@ try:
     cookie_accept_button.click()
 except TimeoutException:
     print("No cookie consent pop-up found or it took too long to load.")
+    
+# Wait for the login elements to be present
+try:
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="BRP_MODAL_CONTAINER"]/div/div[2]/div/div[2]/div/form/div[2]/div/div[2]/input'))
+    )
+except TimeoutException:
+    print("Login page elements took too long to load.")
+    driver.quit()
+    exit()
+    
+# Wait for the remember consent pop-up to be present
+try:
+    cookie_accept_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="BRP_MODAL_CONTAINER"]/div/div[2]/div/div[2]/div/form/div[4]/div/div/div[1]'))  # Update the XPath as needed
+    )
+    cookie_accept_button.click()
+except TimeoutException:
+    print("No remember consent pop-up found or it took too long to load.")
